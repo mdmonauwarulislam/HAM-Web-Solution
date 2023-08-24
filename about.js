8bbaccf0d1764b8970d3344f999c45e57c9ed6c2
Md Monauwarul Islam <121303308+mdmonauwarulislam@users.noreply.github.com>
2023-08-25 01:40:24 +0530

    commit

--- about.js
+++ about.js
@@ -109,3 +109,27 @@ gsap.from("#page4 h1", {
     scrub: 3,
   },
 });
+
+
+
+///////////////////////////////////////////CONTACT US//////////////////////////////////////////
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
